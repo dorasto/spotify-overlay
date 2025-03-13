7e1a984c826ffa5f910b0fbef33d2f22258e5d01
trent-gezel <trent.hopton@gmail.com>
2025-03-13 23:57:46 +1100

    FUCK MY LIFE

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import ThemeShowcase from "@/components/showcase";
 import SpotifyLogin from "@/components/spotify-login";
 
-export default function Home() {
+export default function Page() {
     return (
         <main className="bg-zinc-900">
             <div className="flex flex-col items-center justify-center">
