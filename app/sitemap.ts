9e0d0be0948e6e00a183f9826722b6bf477dc171
trent-gezel <trent.hopton@gmail.com>
2025-04-04 15:02:17 +1100

    chore: sitemap fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,11 +1,10 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const url = process.env.ROOT_DOMAIN || "";
     const now = new Date();
     return [
         {
-            url: url,
+            url: "https://music-overlay.doras.to",
             lastModified: now,
             changeFrequency: "monthly",
             priority: 1,
